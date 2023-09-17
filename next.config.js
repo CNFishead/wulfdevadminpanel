a6dc94f68e719e0540793ae389f53b5222ec47f1
Austin Howard <cnfishead@hotmail.com>
2023-09-16 20:36:50 -0400

    changes to workflow

--- next.config.js
+++ next.config.js
@@ -2,8 +2,9 @@
 const nextConfig = {
   reactStrictMode: true,
   env: {
-    API_URL: process.env.API_URL || 'https://api.truthcasting.com/api/v1',
-    ENV: process.env.ENV,
+    API_URL: process.env.API_URL || 'https://austinhowardapi.azurewebsites.net/api/v1',
+    // ENV: process.env.ENV,
+    ENV: 'production',
   },
 
   //Redirect / to /dash
